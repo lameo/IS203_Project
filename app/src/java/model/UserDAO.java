5b1e975e5c5b9acfcc75a0a70a1c8011ace01509
Ying Xu <xuying2319@gmail.com>
2017-09-14 20:57:56 +0800

    Update UserDAO & ConnectionManager

--- app/src/java/model/UserDAO.java
+++ app/src/java/model/UserDAO.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
 
     /**
@@ -11,7 +16,55 @@ public class UserDAO {
     public UserDAO(){
         
     }
-
+    
+    
+    
+    public static User retrieveUserByName(String username, String password){
+        //username and password checking, change to database when implemented
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+            try{
+                //get a connection to database
+                connection = ConnectionManager.getConnection();            
+                //out.print(connection);
+                //prepare a statement
+                preparedStatement = connection.prepareStatement("select * from demographics where name = ? && password = ?");   
+                
+                //set the parameters
+                preparedStatement.setString(1, username);
+                
+                preparedStatement.setString(2, password);
+                
+                //execute SQL query
+                resultSet = preparedStatement.executeQuery();
+                
+                while(resultSet.next()){
+                    //request.getRequestDispatcher("userPage.jsp").forward(request, response);                    
+                    user = new User(resultSet.getString(1),resultSet.getString(2));
+                }
+                
+                
+                
+                /*if(userName!=null){
+                    User user = new User(userName, password);
+                    String userType = user.validate1(username,password);
+                    if(userType.equals("admin")){
+                        request.getRequestDispatcher("adminPage.jsp").forward(request, response);
+                    }
+                    if(userType.equals("user")){
+                        request.getRequestDispatcher("userPage.jsp").forward(request, response);
+                    }
+                }*/
+                
+                
+                
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+            return user;
+    }
     /**
      *
      * Get user
