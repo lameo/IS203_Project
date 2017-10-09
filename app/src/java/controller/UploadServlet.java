a69d803c7a53104695fdf744e3aeb52c4fcbb7a8
Daniel Ang Yang Lin <ylang.2016@smu.edu.sg>
2017-10-09 18:11:10 +0800

    d

--- app/src/java/controller/UploadServlet.java
+++ app/src/java/controller/UploadServlet.java
@@ -2,7 +2,6 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +11,8 @@ import java.util.*;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import model.UploadDAO;
-//import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 
-
-public class UploadServlet extends HttpServlet implements java.io.Serializable{
+public class UploadServlet extends HttpServlet implements java.io.Serializable {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -26,53 +23,70 @@ public class UploadServlet extends HttpServlet implements java.io.Serializable{
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {      
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();        
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         UploadBean upBean = new UploadBean();
         String success = "";
-        String uploadType = "";     
-        try{
-            //DiskFileItemFactory factory = new DiskFileItemFactory();   
+        String uploadType = "";
+        ArrayList<String[]> demographicsError = new ArrayList<>();
+        //ArrayList<List<String>> locationError = new ArrayList<List<String>>();
+        //ArrayList<List<String>> lookupError = new ArrayList<List<String>>();        
+        try {
             ServletContext servletContext = this.getServletConfig().getServletContext();
             File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir"); //Pathname to a scratch directory to be provided by this Context for temporary read-write use by servlets within the associated web application
-            String outputDirectory = "" + repository; 
-            
+            String outputDirectory = "" + repository;
+
             upBean.setFolderstore(outputDirectory); //the location of where documents will be stored
             Long size = Long.parseLong("8589934592"); //the size limit of the file uploads
             upBean.setFilesizelimit(size);
-        
-            if (MultipartFormDataRequest.isMultipartFormData(request)){
+
+            if (MultipartFormDataRequest.isMultipartFormData(request)) {
                 //Uses MultipartFormDataRequest to parse the HTTP request.
                 MultipartFormDataRequest multipartRequest = new MultipartFormDataRequest(request); //specialized version of request object to interpret the data
                 uploadType = multipartRequest.getParameter("uploadType"); //either intialize or update              
                 String todo = multipartRequest.getParameter("todo");
-                
-                if(todo.equalsIgnoreCase("upload")){
+
+                if (todo.equalsIgnoreCase("upload")) {
                     Hashtable files = multipartRequest.getFiles(); //get the files sent over, hastable is the older version of hashmap
-                    if ((files != null) && (!files.isEmpty())){
+                    if ((files != null) && (!files.isEmpty())) {
                         UploadFile file = (UploadFile) files.get("uploadfile"); //get the files from bootstrapinitialize or bootstrapupdate
-                        if (file != null && file.getFileSize()>0 && file.getFileName()!=null) {
+                        if (file != null && file.getFileSize() > 0 && file.getFileName() != null) {
+                            String fileName = file.getFileName();
                             String contentType = file.getContentType(); //Get the file type
-                            String filePath = outputDirectory + File.separator + file.getFileName(); //get the zip file directory 
-                            
-                            if(contentType.equals("application/x-zip-compressed")){ //if it is a zip file
+                            String filePath = outputDirectory + File.separator + fileName; //get the zip file directory 
+
+                            if (contentType.equals("application/x-zip-compressed")) { //if it is a zip file
                                 upBean.store(multipartRequest, "uploadfile"); //save to directory
-                                
-                                out.print(UploadDAO.unzip(filePath, outputDirectory)); //unzip the files in the zip and save into the directory    
-                                
-                                success = "Uploaded file: " + file.getFileName()+ " (" + file.getFileSize() + " bytes)";
+
+                                String fileExist = UploadDAO.unzip(filePath, outputDirectory); //unzip the files in the zip and save into the directory    
+
+                                if (fileExist != null && fileExist.contains("demographics.csv")) {
+                                    UploadDAO.demographicsImport(outputDirectory + File.separator + "demographics.csv");
+                                    demographicsError = UploadDAO.demographicsNilChecking();
+                                } else if (fileExist != null && fileExist.contains("location.csv")) {
+                                    //locationError = UploadDAO.demographicsImport(outputDirectory + File.separator + "location.csv");
+                                } else if (fileExist != null && fileExist.contains("location-lookup.csv")) {
+                                    //lookupError = UploadDAO.demographicsImport(outputDirectory + File.separator + "location-lookup.csv");
+                                }
+
+                                success = "Uploaded file: " + fileName + " (" + file.getFileSize() + " bytes)";
                                 session.setAttribute("success", success); //send success messsage                                
-                                   
-                            } else if(UploadDAO.checkFileName(file.getFileName())){ //if location.csv or location-lookup.csv or demographics.csv
+
+                            } else if (UploadDAO.checkFileName(fileName) != null && UploadDAO.checkFileName(fileName).length() > 0) { //if location.csv or location-lookup.csv or demographics.csv
                                 upBean.store(multipartRequest, "uploadfile"); //save to directory
-                                
-                                out.print(UploadDAO.readCSV(filePath));
-                                
-                                success = "Uploaded file: " + file.getFileName()+ " (" + file.getFileSize() + " bytes)";
+
+                                if (fileName.equals("demographics.csv")) {
+                                    UploadDAO.demographicsImport(outputDirectory + File.separator + "demographics.csv");
+                                    demographicsError = UploadDAO.demographicsNilChecking();
+                                } else if (fileName.equals("location.csv")) {
+                                    //locationError = UploadDAO.demographicsImport(outputDirectory + File.separator + "location.csv");
+                                } else if (fileName.equals("location-lookup.csv")) {
+                                    //lookupError = UploadDAO.demographicsImport(outputDirectory + File.separator + "location-lookup.csv");
+                                }
+
+                                success = "Uploaded file: " + fileName + " (" + file.getFileSize() + " bytes)";
                                 session.setAttribute("success", success); //send success messsage         
-                                
+
                             } else {
                                 session.setAttribute("error", "Wrong file name or type"); //send error messsage                                  
                             }
@@ -82,17 +96,12 @@ public class UploadServlet extends HttpServlet implements java.io.Serializable{
                     } else {
                         session.setAttribute("error", "No uploaded files"); //send error messsage                   
                     }
-                }            
+                }
             }
-        } catch (UploadException e){
+        } catch (UploadException e) {
             session.setAttribute("error", "Unable to upload. Please try again later"); //send error messsage
-        }    
-        
-        if(uploadType.equals("update")){ //check where did the request come from and send back to the respective place
-            //response.sendRedirect("BootstrapUpdate.jsp");                  
-        } else {
-            //response.sendRedirect("BootstrapInitialize.jsp");     
         }
+        response.sendRedirect("BootstrapInitialize.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- app/src/java/model/UploadDAO.java
+++ app/src/java/model/UploadDAO.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -25,8 +26,15 @@ public class UploadDAO {
             connection = ConnectionManager.getConnection();
 
             //prepare a statement
-            preparedStatement = connection.prepareStatement("DROP DATABASE IF EXISTS data; create schema \"data\"; use \"data\"; create table demographics ( macaddress varchar(40) not null, name varchar(50) not null, password varchar(50) not null, email varchar(50) not null, gender char(1) not null, constraint demographics_pk primary key(macaddress) ); create table locationlookup ( locationid varchar(12) not null, locationname varchar(25) not null, constraint locationlookup_pk primary key(locationid) ); create table location ( timestamp datetime(6) not null,  macaddress varchar(40) not null,  locationid varchar(10) not null,  constraint location_pk primary key(macaddress, timestamp) ); create table stalkerMode ( macaddress varchar(40) not null,  locationid varchar(10) not null,  locationname varchar(25) not null, maxTimestamp datetime(6) not null, minTimestamp datetime(6) not null, minutesSpend int(4) not null, constraint location_pk primary key(macaddress, maxTimestamp) ); SHOW TABLES LIKE \"%demographic%\";");
-
+            preparedStatement = connection.prepareStatement(
+                    "DROP DATABASE IF EXISTS data;\n"
+                    + "create schema `data`;"
+                    + "use `data`;"
+                    + "create table demographics ( macaddress varchar(40) not null, name varchar(50) not null, password varchar(50) not null, email varchar(50) not null, gender char(1) not null, constraint demographics_pk primary key(macaddress) );"
+                    + "create table locationlookup ( locationid varchar(12) not null, locationname varchar(25) not null, constraint locationlookup_pk primary key(locationid) );"
+                    + "create table location ( timestamp varchar(20) not null,  macaddress varchar(40) not null,  locationid varchar(10) not null,  constraint location_pk primary key(macaddress, timestamp) );"
+                    + "create table stalkerMode ( macaddress varchar(40) not null,  locationid varchar(10) not null,  locationname varchar(25) not null, maxTimestamp datetime(6) not null, minTimestamp datetime(6) not null, minutesSpend int(4) not null, constraint location_pk primary key(macaddress, maxTimestamp) );"
+                    + "SHOW TABLES LIKE '%demographic%';");
             //set the parameters
             //nth to prepare
             //execute SQL query
@@ -45,44 +53,22 @@ public class UploadDAO {
         return ans.equals("demographic");
     }
 
-    public static boolean importDemographics(String macAddress, String name, String password, String email, String gender) {
-
-        boolean macValid = macAddress.length() == 40;
-        boolean passwordValid = (password.length() > 8) && password.contains(" ");
-        boolean genderValid = gender.equals("m") || gender.equals("f");
-
-        String[] years = "2013 2014 2015 2016 2017".split(" ");
-        String[] schools = "business accountancy sis economics law socsc".split(" ");
-        boolean emailValid = false;
-        for (String year : years) {
-            for (String school : schools) {
-                String tempo = year + "@" + school;
-                if (email.contains(tempo)) {
-                    emailValid = true;
-                }
-            }
-        }
-
-        if (macValid && passwordValid && genderValid && emailValid) {
-            return false;
-        }
-
+    public static void demographicsImport(String fileLocation) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        String ans = "";
+        ArrayList<String[]> emptyDataSet = new ArrayList<>();
         try {
             //get a connection to database
             connection = ConnectionManager.getConnection();
 
             //prepare a statement
-            preparedStatement = connection.prepareStatement("\"INSERT INTO demographic (?, ?, ?, ?, ?)");
+            preparedStatement = connection.prepareStatement(
+                    "LOAD DATA LOCAL INFILE ? INTO TABLE demographics"
+                    + "FIELDS TERMINATED BY ',' LINES TERMINATED BY '\\r' IGNORE 1 LINES (macaddress, name, password, email, gender);"
+            );
             //set the parameters
-            preparedStatement.setString(1, macAddress);
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, password);
-            preparedStatement.setString(4, email);
-            preparedStatement.setString(5, gender);
+            preparedStatement.setString(1, fileLocation);
 
             //execute SQL query
             resultSet = preparedStatement.executeQuery();
@@ -94,11 +80,40 @@ public class UploadDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return true;
+    }
+
+    public static ArrayList<String[]> demographicsNilChecking() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        ArrayList<String[]> emptyDataSet = new ArrayList<>();
+        try {
+            //get a connection to database
+            connection = ConnectionManager.getConnection();
+
+            //prepare a statement
+            preparedStatement = connection.prepareStatement("select  *  from  demographics where password is null or password like '' or name is null or name like '' or macaddress is null or macaddress like '' or gender is null or gender like '' or email is null or email like ''");
+            //set the parameters
+            //preparedStatement.setString(1, fileLocation);
+
+            //execute SQL query
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                emptyDataSet.add(new String[]{resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5)});
+            }
+
+            //close connections
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return emptyDataSet;
     }
 
     public static String unzip(String zipFile, String outputDirectory) {
-        String test = "";
+        String fileExist = "";
         byte[] buffer = new byte[1024];
         try {
             File folder = new File(outputDirectory);
@@ -111,7 +126,8 @@ public class UploadDAO {
 
             while (ze != null) {
                 String fileName = ze.getName();
-                if (checkFileName(fileName)) { //unzip only the correct csv files
+                if (checkFileName(fileName) != null && checkFileName(fileName).length() > 0) { //unzip only the correct csv files
+                    fileExist += " " + fileName;
                     File newFile = new File(outputDirectory + File.separator + fileName);
 
                     //create all non exists folders else you will hit FileNotFoundException for compressed folder
@@ -123,7 +139,6 @@ public class UploadDAO {
                         fos.write(buffer, 0, len);
                     }
                     fos.close();
-                    test += readCSV(outputDirectory + File.separator + fileName);                      
                 }
                 ze = zis.getNextEntry();
             }
@@ -133,32 +148,17 @@ public class UploadDAO {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return test;        
+        return fileExist;
     }
 
-    public static boolean checkFileName(String fileName) {
+    public static String checkFileName(String fileName) {
         if (fileName.contains("demographics.csv")) {
-            return true;
+            return "demographics.csv";
         } else if (fileName.contains("location.csv")) {
-            return true;
+            return "location.csv";
         } else if (fileName.contains("location-lookup.csv")) {
-            return true;
+            return "location-lookup.csv";
         }
-        return false;
-    }
-
-    public static String readCSV(String filePath) {
-        String test = "";
-        try {
-            CSVReader reader = new CSVReader(new FileReader(filePath));
-            String[] columns;
-            while ((columns = reader.readNext()) != null) {
-                test = columns[0];
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return test;
+        return "";
     }
 }
