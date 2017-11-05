2987313ebd682ee59b96b8bed35769952b925658
Andy <andy.ang.2016@smu.edu.sg>
2017-11-05 09:55:25 +0800

    aaaaaaaaaaa

--- app/src/java/controller/HeatMapServlet.java
+++ app/src/java/controller/HeatMapServlet.java
@@ -7,37 +7,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
+import java.text.ParseException;
 import model.HeatMapDAO;
 import java.util.Date;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.HeatMap;
 
 public class HeatMapServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession();
+        try {
+            HttpSession session = request.getSession();
+            String timeDate = request.getParameter("timeDate"); //retrieve time from user input
+            timeDate = timeDate.replace("T", " ");
+            SimpleDateFormat readFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            SimpleDateFormat writeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            
+            Date timestamp = (Date) readFormat.parse(timeDate);
+            timeDate = writeFormat.format(timestamp);
+            int floor = Integer.parseInt(request.getParameter("floor")); //retrieve floor from user input
+            String floorName = "B1";
 
-        String timeDate = request.getParameter("timeDate"); //retrieve time from user input
-        timeDate = timeDate.replace("T", " ");
-        SimpleDateFormat readFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        SimpleDateFormat writeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date timestamp = null;
+            if (floor > 0) {
+                floorName = "L" + floor;
+            }
 
-        int floor = Integer.parseInt(request.getParameter("floor")); //retrieve floor from user input
-        String floorName = "B1";
+            Map<String, HeatMap> heatmapList = HeatMapDAO.retrieveHeatMap(timeDate, floorName);
+            session.setAttribute("heatmapList", heatmapList);
 
-        if (floor > 0) {
-            floorName = "L" + floor;
+            session.setAttribute("floorName", floorName);
+            session.setAttribute("timeDate", timeDate);
+
+            response.sendRedirect("heatmapPage.jsp"); //send back to heatmapPage
+        } catch (ParseException ex) {
+            Logger.getLogger(ReportServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        Map<String, HeatMap> heatmapList = HeatMapDAO.retrieveHeatMap(timeDate, floorName);
-        session.setAttribute("heatmapList", heatmapList);
-
-        session.setAttribute("floorName", floorName);
-        session.setAttribute("timeDate", timeDate);
-
-        response.sendRedirect("heatmapPage.jsp"); //send back to heatmapPage
 
     }
 
