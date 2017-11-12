4aac97fca3861bf3aa0621cdd4727e12429ee7cd
Andy <andy.ang.2016@smu.edu.sg>
2017-11-12 17:27:04 +0800

    aaaaaaaaa

--- app/sql/data.sql
+++ app/sql/data.sql
@@ -1,62 +1,87 @@
-create schema `data`;
-use `data`;
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2017 at 09:15 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
-#demographics
-create table demographics
-(
-macaddress varchar(40) ,
-name varchar(50) ,
-password varchar(50) ,
-email varchar(50) ,
-gender char(1) not null,
-constraint demographics_pk primary key(macaddress)
-);
-
-LOAD DATA LOCAL INFILE 'D:/testt/app/web/resource/data/demographics.csv' INTO TABLE demographics
-FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (macaddress, name, password, email, gender);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-create table locationlookup
-(
-locationid varchar(12) not null,
-locationname varchar(40) not null,
-constraint locationlookup_pk primary key(locationid)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-LOAD DATA LOCAL INFILE 'D:/testt/app/web/resource/data/location-lookup.csv' INTO TABLE locationlookup
-FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (locationid, locationname);
+--
+-- Database: `data`
+--
 
+-- --------------------------------------------------------
 
-create table location
-(
-timestamp datetime(6) ,
-macaddress varchar(40) ,
-locationid varchar(10) ,
-constraint location_pk primary key(macaddress, timestamp)
-);
+--
+-- Table structure for table `demographics`
+--
 
-LOAD DATA LOCAL INFILE 'D:/testt/app/web/resource/data/location.csv' INTO TABLE location
-FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (timestamp, macaddress, locationid);
+DROP TABLE IF EXISTS `demographics`;
+CREATE TABLE `demographics` (
+  `macaddress` varchar(40) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` char(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-create table stalkerMode
-(
-macaddress varchar(40) ,
-locationid varchar(10) ,
-locationname varchar(25) not null,
-maxTimestamp datetime(6) not null,
-minTimestamp datetime(6) not null,
-minutesSpent int(4) not null,
-constraint location_pk primary key(macaddress, maxTimestamp)
-);
+--
+-- Table structure for table `location`
+--
 
-INSERT INTO stalkerMode ()
-SELECT 
-	macaddress, location.locationid, locationname, max(timestamp), min(timestamp), TIMESTAMPDIFF(MINUTE, min(timestamp),max(timestamp))
-from 
-	location, 
-    locationlookup 
-where 
-	location.locationid = locationlookup.locationid
-group by 
-	macaddress, location.locationid, locationname, day(timestamp);
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location` (
+  `timestamp` datetime NOT NULL,
+  `macaddress` varchar(40) NOT NULL,
+  `locationid` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locationlookup`
+--
+
+DROP TABLE IF EXISTS `locationlookup`;
+CREATE TABLE `locationlookup` (
+  `locationid` varchar(10) NOT NULL,
+  `locationname` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `demographics`
+--
+ALTER TABLE `demographics`
+  ADD PRIMARY KEY (`macaddress`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`macaddress`,`timestamp`);
+
+--
+-- Indexes for table `locationlookup`
+--
+ALTER TABLE `locationlookup`
+  ADD PRIMARY KEY (`locationid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
