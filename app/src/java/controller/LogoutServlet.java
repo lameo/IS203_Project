4860e032adc88dc302270b8a3478a3ef811aa08f
Andy <andy.ang.2016@smu.edu.sg>
2017-11-15 22:30:44 +0800

    one more

--- app/src/java/controller/LogoutServlet.java
+++ app/src/java/controller/LogoutServlet.java
@@ -8,7 +8,14 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
-
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // retrieve session containing a user object
         HttpSession session = request.getSession();
