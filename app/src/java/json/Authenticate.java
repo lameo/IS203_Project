3cfb736327fb2e26e43def44d7aa46023595ddd0
Andy <andy.ang.2016@smu.edu.sg>
2017-11-16 22:18:15 +0800

    .

--- app/src/java/json/Authenticate.java
+++ app/src/java/json/Authenticate.java
@@ -25,6 +25,15 @@ import model.User;
 @WebServlet(urlPatterns = {"/json/authenticate"})
 public class Authenticate extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
--- app/src/java/json/Breakdown.java
+++ app/src/java/json/Breakdown.java
@@ -23,6 +23,15 @@ import model.SharedSecretManager;
 @WebServlet(urlPatterns = {"/json/basic-loc-report"})
 public class Breakdown extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
